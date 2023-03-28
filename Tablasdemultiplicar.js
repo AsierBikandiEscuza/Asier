72245c81e80aa38ce3432e2dd022af4deec2b272
AsierBikandiEscuza <127678375+AsierBikandiEscuza@users.noreply.github.com>
2023-03-28 19:39:53 +0200

    Delete Tablasdemultiplicar.js

--- Tablasdemultiplicar.js
+++
@@ -1,7 +0,0 @@
-const multiplicador = parseInt(
-    prompt("Introduce un numero para ver su tabla de multiplicar")
-);
-console.log(multiplicador);
-console.log("tabla del", multiplicador);
-console.log("---------------");
-console.log(`1x ${multiplicador} = ${1 * multiplicador}`);
