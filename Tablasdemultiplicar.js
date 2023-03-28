c65032d02f390caa3956dc82b33db9ecbb7913fa
AsierBikandiEscuza <127678375+AsierBikandiEscuza@users.noreply.github.com>
2023-03-28 19:38:15 +0200

    Add files via upload

---
+++ Tablasdemultiplicar.js
@@ -0,0 +1,7 @@
+const multiplicador = parseInt(
+    prompt("Introduce un numero para ver su tabla de multiplicar")
+);
+console.log(multiplicador);
+console.log("tabla del", multiplicador);
+console.log("---------------");
+console.log(`1x ${multiplicador} = ${1 * multiplicador}`);
